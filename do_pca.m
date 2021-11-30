5c5dac725208ceb299bf2e442ac8c9d1ab4921c2
April Sin <jazziblue@gmail.com>
2021-11-29 19:30:11 -0800

    run script on more hyperspectral images

--- do_pca.m
+++ do_pca.m
@@ -1,11 +1,22 @@
-load("L_data.mat");
-load("M_data.mat");
-load("S_data.mat");
-load("Q_data.mat");
+% load("L_data.mat");
+% load("M_data.mat");
+% load("S_data.mat");
+% load("Q_data.mat");
+% 
+% L_log = log10(L) - mean(log10(L));
+% M_log = log10(M) - mean(log10(M));
+% S_log = log10(S) - mean(log10(S));
+% Q_log = log10(Q) - mean(log10(Q));
+% 
+% coeff = pca([L_log(:), M_log(:), S_log(:), Q_log(:)]);
 
-L_log = log10(L) - mean(log10(L));
-M_log = log10(M) - mean(log10(M));
-S_log = log10(S) - mean(log10(S));
-Q_log = log10(Q) - mean(log10(Q));
+coeff_mean = zeros(4, 4);
 
-coeff = pca([L_log(:), M_log(:), S_log(:), Q_log(:)]);
+for i = [1,2,3,4,6,7,8]
+    name = strcat("scene", int2str(i), "_coeff.mat");
+    load(name); 
+    coeff_mean = coeff_mean + coeff;
+end
+
+coeff_mean = coeff_mean / 7.0;
+save('coeff_mean.mat', "coeff_mean");
--- scatter_plot.m
+++ scatter_plot.m
@@ -1,65 +1,77 @@
-load("radiances_25000.mat");
-load("large_radiances.mat");
-r = radiances;
-
-load("S_cone.mat");
-load("M_cone.mat");
-load("L_cone.mat");
-load("Q_cone.mat");
-
-data_points = zeros(255, 335, 4);
-
-for i = 1:255
-    for j = 1:335
-         data_points(i, j, 1) = sum(squeeze(r(i, j, :))' .* L_cone);
-         data_points(i, j, 2) = sum(squeeze(r(i, j, :))' .* M_cone);
-         data_points(i, j, 3) = sum(squeeze(r(i, j, :))' .* S_cone);
-         data_points(i, j, 4) = sum(squeeze(r(i, j, :))' .* Q_cone2);
+for s = [1, 2, 3, 4, 6, 7, 8]
+    name = strcat("scene", int2str(s));
+    load(strcat("hyperspectral_images/", name, "_radiances.mat"));
+    r = radiances;
+    
+    load("S_cone.mat");
+    load("M_cone.mat");
+    load("L_cone.mat");
+    load("Q_cone.mat");
+    
+    h = 1000;
+    w = 1000;
+    data_points = zeros(h, w, 4);
+    
+    for i = 1:h
+        for j = 1:w
+             data_points(i, j, 1) = sum(squeeze(r(i, j, :))' .* L_cone);
+             data_points(i, j, 2) = sum(squeeze(r(i, j, :))' .* M_cone);
+             data_points(i, j, 3) = sum(squeeze(r(i, j, :))' .* S_cone);
+             data_points(i, j, 4) = sum(squeeze(r(i, j, :))' .* Q_cone2);
+        end
     end
+    
+    save(strcat(name, '_data_points', '.mat'), 'data_points');
+    
+    L = reshape(data_points(:, :, 1).', 1, []);
+    M = reshape(data_points(:, :, 2).', 1, []);
+    S = reshape(data_points(:, :, 3).', 1, []);
+    Q = reshape(data_points(:, :, 4).', 1, []);
+    
+    save(strcat(name, '_L_data.mat'), 'L');
+    save(strcat(name, '_M_data.mat'), 'M');
+    save(strcat(name, '_S_data.mat'), 'S');
+    save(strcat(name, '_Q_data.mat'), 'Q');
+    
+    fig1 = figure();
+    hold on;
+    scatter(L(1:500), M(1:500));
+    xlabel("L");
+    ylabel("M");
+    saveas(fig1, strcat(name, '_L-M.png'));
+    hold off;
+    
+    fig2 = figure();
+    hold on;
+    scatter(L(1:500), Q(1:500));
+    xlabel("L");
+    ylabel("Q");
+    saveas(fig1, strcat(name, '_L-Q.png'));
+    hold off;
+    
+    fig3 = figure();
+    hold on;
+    scatter(L(1:500), S(1:500));
+    xlabel("L");
+    ylabel("S");
+    saveas(fig1, strcat(name, '_L-S.png'));
+    hold off;
+    
+    fig4 = figure();
+    hold on;
+    scatter(M(1:500), Q(1:500));
+    xlabel("M");
+    ylabel("Q");
+    saveas(fig1, strcat(name, '_M-Q.png'));
+    hold off;
+    
+    L_log = log10(L) - mean(log10(L));
+    M_log = log10(M) - mean(log10(M));
+    S_log = log10(S) - mean(log10(S));
+    Q_log = log10(Q) - mean(log10(Q));
+    
+    coeff = pca([L_log(:), M_log(:), S_log(:), Q_log(:)]);
+    save(strcat(name, '_coeff.mat'), 'coeff');
 end
-
-save('data_points.mat', 'data_points');
-
-L = reshape(data_points(:, :, 1).', 1, []);
-M = reshape(data_points(:, :, 2).', 1, []);
-S = reshape(data_points(:, :, 3).', 1, []);
-Q = reshape(data_points(:, :, 4).', 1, []);
-
-save('L_data.mat', 'L');
-save('M_data.mat', 'M');
-save('S_data.mat', 'S');
-save('Q_data.mat', 'Q');
-
-fig1 = figure();
-hold on;
-scatter(L(1:500), M(1:500));
-xlabel("L");
-ylabel("M");
-saveas(fig1, 'L-M.png');
-hold off;
-
-fig2 = figure();
-hold on;
-scatter(L(1:500), Q(1:500));
-xlabel("L");
-ylabel("Q");
-saveas(fig1, 'L-Q.png');
-hold off;
-
-fig3 = figure();
-hold on;
-scatter(L(1:500), S(1:500));
-xlabel("L");
-ylabel("S");
-saveas(fig1, 'L-S.png');
-hold off;
-
-fig4 = figure();
-hold on;
-scatter(M(1:500), Q(1:500));
-xlabel("M");
-ylabel("Q");
-saveas(fig1, 'M-Q.png');
-hold off;
 
 
